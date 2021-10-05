25bc0fa949712f98c4b5c2a85fc17a87b8ea0bb6
Salem <owenpnstockdale@gmail.com>
2021-10-05 14:46:42 -0600

    continue text adventure

--- text adventure/main.js
+++ text adventure/main.js
@@ -3,33 +3,119 @@
 
 // global variables
 let game = document.getElementById("game");
-let prompt = document.getElementsByClassName("activePrompt")[0];
-let response = document.getElementsByClassName("activeResponse")[0];
+let dagger = false;
 
 // add event listeners
 document.getElementById("start").addEventListener("click", startGame);
-document.addEventListener("keydown", keydownHandler);
 
 // event functions
-function startGame() {
-    game.style.display="block";
 
-    let promptPreset = Math.random();
-    console.log(promptPreset);
+// game
+function startGame() { 
+    let ans = prompt("You awake in a sewer with tunnels to the east and west.");
 
-    if (promptPreset > 0.5) {
-        prompt.innerHTML = "You are in a dark sewer with paths to your left and right.";
-    } else {
-        prompt.innerHTML = "the other one";
+    if (ans == "go east") {
+        stage0();
+    } else if (ans == "go west") {
+        stage1();
     }
-}
 
-function keydownHandler(event) {
-    if (event.code == "Enter") {
-        response.disabled = true;
-        prompt.classList.remove("activePrompt");
-        response.classList.remove("activeResponse");
+    
+    function stage0() {
+        ans = prompt("You come to a large open room. You hear eerie noises coming from the north end.");
+         
+        if (ans == "go east" || ans == "go south") {
+            alert("There's a wall");
+            stage0();
 
+        } else if (ans == "go north") {
+            stage00();
+        } else if (ans == "go west") {
+            startGame();
+        } else {
+            alert("Unknown command.")
+            stage0();
+        }
+    }
+
+    function stage00() {
+        ans = prompt("After walking for about a minute, you see a sillhouette through the darkness.")
+
+        if (ans == "go north") {
+            stage000();
+        } else if (ans == "go south") {
+            alert("You walk back.");
+            stage0();
+        } else if (ans == "go west" || ans == "go east") {
+            alert("There's a wall.");
+            stage00();
+        } else if (ans == "attack sillhouette") {
+            stage001();
+        } else {
+            alert("Unknown command.");
+            stage00();
+        }
+    }
+
+    function stage000() {
+        ans = prompt("As you get closer the sillhouette fades away. You see you're at the end of the room. There is a tunnel to the east and north.");
+        if (ans == "go north") {
+            stage0000();
+        } else if (ans == "go east") {
+            stage0001();
+        } else {
+            alert("Unknown command.");
+            stage000();
+        }
+    }
+
+    function stage001() {
+        if (dagger == true) {
+            alert("You lunge at the sillhouette with your dagger. It attempts to parry the blow but fails.");
+            ans = confirm("Pull back the hood?");
+            if (ans == true) {
+                alert("The person has a misshapen face. They have nothing of use on them.");     
+                stage0010();           
+            } else {
+                stage0010();
+            }
+        } else {
+            alert("You lunge at the sillhouette. It sees you coming, parries the blow and knocks you out. Game over.");
+        }
+    }
+
+    function stage0010(){
+        ans = prompt("You see a tunnel to the north and east.")
+        if (ans == "go north") {
+            stage0000();
+        } else if (ans == "go east") {
+            stage0001();
+        } else {
+            alert("Unknown command.");      
+        }
+    }
+
+    function stage0000() {
+        alert("You see light at the end of the tunnel. You win.");
+    }
+
+    function stage0001() {
+        alert("You come through and see the sillhouette once again. It lunges at you and knocks you out. Game over.");
+    }
+
+    function stage1() {
+        ans = prompt("You come out into a room. You see a dagger on the floor.")
+
+        if (ans == "take dagger") {
+            dagger = true;
+            alert("You pick up the dagger.");
+            stage1();
+        } else if (ans == "go east") {
+            startGame();
+        }
         
     }
+        
+    
 }
+

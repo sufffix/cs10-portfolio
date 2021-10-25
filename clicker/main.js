8cbc8e0efa22a34eefcdc395fad8ca78a35676ea
Salem <owenpnstockdale@gmail.com>
2021-10-25 14:41:42 -0600

    completely rebranded to cookie clicker, changed names of all values, added rif ctpc + ffg

--- clicker/main.js
+++ clicker/main.js
@@ -21,7 +21,6 @@ let devMenuDisplay = "none";
 
 // add event listeners
 clicker.addEventListener("click", onClick);
-upgradeBtn.addEventListener("click", upgradeClicker);
 inputScoreBtn.addEventListener("click", inputScore);
 gvWkBtn.addEventListener("click", give10Workers);
 
@@ -31,44 +30,8 @@ addEventListener("keyup", keyUpHandler);
 
 // event functions
 function onClick() { // main clicker handler
-    score += clickVal;
+    score += clickVal * hasRIF * hasCTPC;
     scorePar.innerHTML = score.toLocaleString();
-}
-
-function upgradeClicker() { // upgrades clicker to next level
-    if (clickVal == 1 && score >= 50) { // could probably be cleaner
-        clickVal = 2;
-        score -= 50;
-        scorePar.innerHTML = score.toLocaleString();
-        clicker.src = "img/loaf.png";
-        upgradeBtn.innerHTML = "Upgrade to Donut";
-        upgradeBtn.title = "Cost: 250";
-    } else if (clickVal == 2 && score >= 250) {
-        clickVal = 4;
-        score -= 250;
-        scorePar.innerHTML = score.toLocaleString();
-        clicker.src = "img/donut.png";
-        upgradeBtn.innerHTML = "Upgrade to Muffin";
-        upgradeBtn.title = "Cost: 5000";
-    } else if (clickVal == 4 && score >= 5000) {
-        clickVal = 8;
-        score -= 5000;
-        scorePar.innerHTML = score.toLocaleString();
-        clicker.src = "img/muffin.png";
-        upgradeBtn.innerHTML = "Upgrade to Cake";
-        upgradeBtn.title = "Cost: 50000";
-    } else if (clickVal == 8 && score >= 50000) {
-        clickVal = 16;
-        score -= 50000;
-        scorePar.innerHTML = score.toLocaleString();
-        clicker.src = "img/cake.png";
-        upgradeBtn.innerHTML = "Fully upgraded!";
-        upgradeBtn.disabled = true;
-        upgradeBtn.title = "Fully upgraded!";
-    } else {
-        alert("Not enough money!"); // maybe dont use alert? stops other functions while alert open
-    }
-    
 }
 
 function keyDownHandler() { // handles key down presses
@@ -99,268 +62,180 @@ function inputScore() { // input score from dev menu
 
 function give10Workers() {
     for (let i = 1; i <= 10; i++) {
-        bkNum++;
-        bkPar.innerHTML = bkNum;
-        bkCost = Math.round(bkCost * 1.15);
-        bkBtn.title = "Cost:" + bkCost.toLocaleString();
-        bkBtn.disabled = false;
+        cursorNum++;
+        cursorPar.innerHTML = cursorNum;
+        cursorCost = Math.round(cursorCost * 1.15);
+        cursorBtn.title = "Cost:" + cursorCost.toLocaleString();
+        cursorBtn.disabled = false;
 
-        hbNum++;
-        hbPar.innerHTML = hbNum;
-        hbCost = Math.round(hbCost * 1.15);
-        hbBtn.title = "Cost:" + hbCost.toLocaleString();
-        hbBtn.disabled = false;
+        grandmaNum++;
+        grandmaPar.innerHTML = grandmaNum;
+        grandmaCost = Math.round(grandmaCost * 1.15);
+        grandmaBtn.title = "Cost:" + grandmaCost.toLocaleString();
+        grandmaBtn.disabled = false;
 
-        lbNum++;
-        lbPar.innerHTML = lbNum;
-        lbCost = Math.round(lbCost * 1.15);
-        lbBtn.title = "Cost:" + lbCost.toLocaleString();
-        lbBtn.disabled = false;
+        farmNum++;
+        farmPar.innerHTML = farmNum;
+        farmCost = Math.round(farmCost * 1.15);
+        farmBtn.title = "Cost:" + farmCost.toLocaleString();
+        farmBtn.disabled = false;
 
-        cfbNum++;
-        cfbPar.innerHTML = cfbNum;
-        cfbCost = Math.round(cfbCost * 1.15);
-        cfbBtn.title = "Cost:" + cfbCost.toLocaleString();
-        cfbBtn.disabled = false;
-
-        lcoNum++;
-        lcoPar.innerHTML = lcoNum;
-        lcoCost = Math.round(lcoCost * 1.15);
-        lcoBtn.title = "Cost:" + lcoCost.toLocaleString();
-        lcoBtn.disabled = false;
-
-        pcoNum++;
-        pcoPar.innerHTML = pcoNum;
-        pcoCost = Math.round(pcoCost * 1.15);
-        pcoBtn.title = "Cost:" + pcoCost.toLocaleString();
-        pcoBtn.disabled = false;
-
-        ccoNum++;
-        ccoPar.innerHTML = ccoNum;
-        ccoCost = Math.round(ccoCost * 1.15);
-        ccoBtn.title = "Cost:" + ccoCost.toLocaleString();
-        ccoBtn.disabled = false;
-
-        wcoNum++;
-        wcoPar.innerHTML = wcoNum;
-        wcoCost = Math.round(wcoCost * 1.15);
-        wcoBtn.title = "Cost:" + wcoCost.toLocaleString();
-        wcoBtn.disabled = false;
-
-        msbNum++;
-        msbPar.innerHTML = msbNum;
-        msbCost = Math.round(msbCost * 1.15);
-        msbBtn.title = "Cost:" + msbCost.toLocaleString();
-        msbBtn.disabled = false;
+        mineNum++;
+        minePar.innerHTML = mineNum;
+        mineCost = Math.round(mineCost * 1.15);
+        mineBtn.title = "Cost:" + mineCost.toLocaleString();
+        mineBtn.disabled = false;
     }
 }
 
 
-// bakers kid
-let bkBtn = document.getElementById("bkBtn"); // variables
-let bkPar = document.getElementById("bkPar");
-let bkNum = 0;
-let bkCost = 15;
-bkBtn.addEventListener("click", buyBakersKid); // event listener
-function buyBakersKid() { // buy 1 
-    if (score >= bkCost) {
-        score -= bkCost;
+// cursor
+let cursorBtn = document.getElementById("cursorBtn"); // variables
+let cursorPar = document.getElementById("cursorPar");
+let cursorNum = 0;
+let cursorCost = 15;
+cursorBtn.addEventListener("click", buyCursor); // event listener
+function buyCursor() { // buy 1 
+    if (score >= cursorCost) {
+        score -= cursorCost;
         scorePar.innerHTML = score.toLocaleString();
-        bkNum++;
-        bkPar.innerHTML = bkNum;
-        bkCost = Math.round(bkCost * 1.15);
-        bkBtn.title = "Cost: " + bkCost.toLocaleString();
-        if (bkNum >= 1) {
-            let miniChefHatBtn = document.createElement("button");
-            miniChefHatBtn.innerHTML = "Buy Mini Chef Hat";
-            miniChefHatBtn.title = "Mouse and baker's kids are twice as efficient."
-            gameR.appendChild(miniChefHatBtn);
-            
+        cursorNum++;
+        cursorPar.innerHTML = cursorNum;
+        cursorCost = Math.round(cursorCost * 1.15);
+        cursorBtn.title = "Cost: " + cursorCost.toLocaleString();
+        if (cursorNum >= 1) {
+            rifBtn.style.display = "block";
+            ctpcBtn.style.display = "block";
+
         }
-        if (hbBtn.disabled == true) {hbBtn.disabled = false};
+        if (grandmaBtn.disabled == true) {grandmaBtn.disabled = false};
     } else {
         alert("Not enough money!")
     }
 }
 
-// home baker
-let hbBtn = document.getElementById("hbBtn"); // variables
-let hbPar = document.getElementById("hbPar");
-let hbNum = 0;
-let hbCost = 100;
-hbBtn.addEventListener("click", buyHomeBaker); // event listener
-function buyHomeBaker() { // buy 1 
-    if (score >= hbCost) {
-        score -= hbCost;
+// grandma
+let grandmaBtn = document.getElementById("grandmaBtn"); // variables
+let grandmaPar = document.getElementById("grandmaPar");
+let grandmaNum = 0;
+let grandmaCost = 100;
+grandmaBtn.addEventListener("click", buyGrandma); // event listener
+function buyGrandma() { // buy 1 
+    if (score >= grandmaCost) {
+        score -= grandmaCost;
         scorePar.innerHTML = score.toLocaleString();
-        hbNum++;
-        hbPar.innerHTML = hbNum;
-        hbCost = Math.round(hbCost * 1.15);
-        hbBtn.title = "Cost: " + hbCost.toLocaleString();
-        if (lbBtn.disabled == true) {lbBtn.disabled = false};
+        grandmaNum++;
+        grandmaPar.innerHTML = grandmaNum;
+        grandmaCost = Math.round(grandmaCost * 1.15);
+        grandmaBtn.title = "Cost: " + grandmaCost.toLocaleString();
+        if (grandmaNum >= 1) {
+            ffgBtn.style.display = "block";
+        }
+        if (farmBtn.disabled == true) {farmBtn.disabled = false};
     } else {
         alert("Not enough money!")
     }
 }
 
-// local baker
-let lbBtn = document.getElementById("lbBtn"); // variables
-let lbPar = document.getElementById("lbPar");
-let lbNum = 0;
-let lbCost = 1100;
-lbBtn.addEventListener("click", buyLocalBaker); // event listener
-function buyLocalBaker() { // buy 
-    if (score >= lbCost) {
-        score -= lbCost;
+// farm
+let farmBtn = document.getElementById("farmBtn"); // variables
+let farmPar = document.getElementById("farmPar");
+let farmNum = 0;
+let farmCost = 1100;
+farmBtn.addEventListener("click", buyFarm); // event listener
+function buyFarm() { // buy 
+    if (score >= farmCost) {
+        score -= farmCost;
         scorePar.innerHTML = score.toLocaleString();
-        lbNum++;
-        lbPar.innerHTML = lbNum;
-        lbCost = Math.round(lbCost * 1.15);
-        lbBtn.title = "Cost: " + lbCost.toLocaleString();
-        if (cfbBtn.disabled == true) {cfbBtn.disabled = false};
+        farmNum++;
+        farmPar.innerHTML = farmNum;
+        farmCost = Math.round(farmCost * 1.15);
+        farmBtn.title = "Cost: " + farmCost.toLocaleString();
+        if (mineBtn.disabled == true) {mineBtn.disabled = false};
     } else {
         alert("Not enough money!")
     }
 }
 
-// city famous baker
-let cfbBtn = document.getElementById("cfbBtn"); // variables
-let cfbPar = document.getElementById("cfbPar");
-let cfbNum = 0;
-let cfbCost = 12000;
-cfbBtn.addEventListener("click", buyCityFamousBaker); // event listener
-function buyCityFamousBaker() { // buy 1 
-    if (score >= cfbCost) {
-        score -= cfbCost;
+// mine
+let mineBtn = document.getElementById("mineBtn"); // variables
+let minePar = document.getElementById("minePar");
+let mineNum = 0;
+let mineCost = 12000;
+mineBtn.addEventListener("click", buyMine); // event listener
+function buyMine() { // buy 1 
+    if (score >= mineCost) {
+        score -= mineCost;
         scorePar.innerHTML = score.toLocaleString();
-        cfbNum++;
-        cfbPar.innerHTML = cfbNum;
-        cfbCost = Math.round(cfbCost * 1.15);
-        cfbBtn.title = "Cost: " + cfbCost.toLocaleString();
-        if (lcoBtn.disabled == true) {lcoBtn.disabled = false};
+        mineNum++;
+        minePar.innerHTML = mineNum;
+        mineCost = Math.round(mineCost * 1.15);
+        mineBtn.title = "Cost: " + mineCost.toLocaleString();
+        // if (lcoBtn.disabled == true) {lcoBtn.disabled = false};
     } else {
         alert("Not enough money!")
     }
 }
 
-// local chain owner
-let lcoBtn = document.getElementById("lcoBtn"); // variables
-let lcoPar = document.getElementById("lcoPar");
-let lcoNum = 0;
-let lcoCost = 130000;
-lcoBtn.addEventListener("click", buyLocalChainOwner); // event listener
-function buyLocalChainOwner() { // buy 1 
-    if (score >= lcoCost) {
-        score -= lcoCost;
-        scorePar.innerHTML = score.toLocaleString();
-        lcoNum++;
-        lcoPar.innerHTML = lcoNum;
-        lcoCost = Math.round(lcoCost * 1.15);
-        lcoBtn.title = "Cost: " + lcoCost.toLocaleString();
-        if (pcoBtn.disabled == true) {pcoBtn.disabled = false};
-    } else {
-        alert("Not enough money!")
-    }
-}
 
-// province wide chain owner
-let pcoBtn = document.getElementById("pcoBtn"); // variables
-let pcoPar = document.getElementById("pcoPar");
-let pcoNum = 0;
-let pcoCost = 1400000;
-pcoBtn.addEventListener("click", buyProvinceChainOwner); // event listener
-function buyProvinceChainOwner() { // buy 1 
-    if (score >= pcoCost) {
-        score -= pcoCost;
-        scorePar.innerHTML = score.toLocaleString();
-        pcoNum++;
-        pcoPar.innerHTML = pcoNum;
-        pcoCost = Math.round(pcoCost * 1.15);
-        pcoBtn.title = "Cost: " + pcoCost.toLocaleString();
-        if (ccoBtn.disabled == true) {ccoBtn.disabled = false};
-    } else {
-        alert("Not enough money!")
-    }
-}
-
-// country wide chain owner
-let ccoBtn = document.getElementById("ccoBtn"); // variables
-let ccoPar = document.getElementById("ccoPar");
-let ccoNum = 0;
-let ccoCost = 20000000;
-ccoBtn.addEventListener("click", buyCountryChainOwner); // event listener
-function buyCountryChainOwner() { // buy 1
-    if (score >= ccoCost) {
-        score -= ccoCost;
-        scorePar.innerHTML = score.toLocaleString();
-        ccoNum++;
-        ccoPar.innerHTML = ccoNum;
-        ccoCost = Math.round(ccoCost * 1.15);
-        ccoBtn.title = "Cost: " + ccoCost.toLocaleString();
-        if (wcoBtn.disabled == true) {wcoBtn.disabled = false};
-    } else {
-        alert("Not enough money!")
-    }
-}
-
-// worldwide chain owner
-let wcoBtn = document.getElementById("wcoBtn"); // variables
-let wcoPar = document.getElementById("wcoPar");
-let wcoNum = 0;
-let wcoCost = 330000000;
-wcoBtn.addEventListener("click", buyWorldChainOwner); // event listener
-function buyWorldChainOwner() { // buy 1
-    if (score >= wcoCost) {
-        score -= wcoCost;
-        scorePar.innerHTML = score.toLocaleString();
-        wcoNum++;
-        wcoPar.innerHTML = wcoNum;
-        wcoCost = Math.round(wcoCost * 1.15);
-        wcoBtn.title = "Cost: " + wcoCost.toLocaleString();
-        if (msbBtn.disabled == true) {msbBtn.disabled = false};
-    } else {
-        alert("Not enough money!")
-    }
-}
-
-// michelin star baker
-let msbBtn = document.getElementById("msbBtn"); // variables
-let msbPar = document.getElementById("msbPar");
-let msbNum = 0;
-let msbCost = 5100000000;
-msbBtn.addEventListener("click", buyMichelinStarBaker); // event listener
-function buyMichelinStarBaker() { // buy 1
-    if (score >= msbCost) {
-        score -= msbCost;
-        scorePar.innerHTML = score.toLocaleString();
-        msbNum++;
-        msbPar.innerHTML = msbNum;
-        msbCost = Math.round(msbCost * 1.15);
-        msbBtn.title = "Cost: " + msbCost.toLocaleString();
-        // if (hbBtn.disabled == true) {hbBtn.disabled = false};
-    } else {
-        alert("Not enough money!")
-    }
-}
 // give score for employees
 setInterval(function(){ // bakers kid function because i didnt want to use decimals in the one second function and im too lazy to find a better way
-    score += bkNum;
+    score += cursorNum * hasRIF * hasCTPC;
     scorePar.innerHTML = score.toLocaleString();
 }, 10000)
 setInterval(function(){ // same thing as bakers kid but for home bakers
-    score += hbNum;
+    score += grandmaNum * hasFFG;
     scorePar.innerHTML = score.toLocaleString();
 }, 1000)
 
 setInterval(function(){ // divide cps by 10 (fires 10 times / second)
-    score += lbNum;
-    score += cfbNum * 5; 
-    score += lcoNum * 26;
-    score += pcoNum * 140;
-    score += ccoNum * 780;
-    score += wcoNum * 4400;
-    score += msbNum * 26000
+    score += farmNum;
+    score += mineNum * 5; 
 
     scorePar.innerHTML = score.toLocaleString();
-
 }, 100);
+
+// upgrades
+
+// buy reinforced index finger
+let rifBtn = document.getElementById("rifBtn");
+let hasRIF = 1;
+rifBtn.addEventListener("click", buyRIF);
+function buyRIF() {
+    if (score >= 100) {
+        score -= 100;
+        hasRIF = 2;
+        rifBtn.disabled = true;
+    } else {
+        alert("Not enough money!");
+    }
+}
+
+// buy carpal tunnel prevention cream
+let ctpcBtn = document.getElementById("ctpcBtn");
+let hasCTPC = 1;
+ctpcBtn.addEventListener("click", buyCTPC);
+function buyCTPC() {
+    if (score >= 500) {
+        score -= 500;
+        hasCTPC = 2;
+        ctpcBtn.disabled = true;
+    } else {
+        alert("Not enough money!");
+    }
+}
+
+// buy forwards from grandma
+let ffgBtn = document.getElementById("ffgBtn");
+let hasFFG = 1;
+ffgBtn.addEventListener("click", buyFFG);
+function buyFFG() {
+    if (score >= 1000) {
+        score -= 1000;
+        hasFFG = 2;
+        ffgBtn.disabled = true;
+    } else {
+        alert("Not enough money!");
+    }
+}
+
